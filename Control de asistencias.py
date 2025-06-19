1b7a960c7ca6b32f7ec0cd64827eedbb46d6b9e1
Zempoalteca Erik Alonso <105683922+Zempoalteca-Erik-Alonso@users.noreply.github.com>
2025-06-19 16:27:05 -0600

    Update Control de asistencias.py

--- Control de asistencias.py
+++ Control de asistencias.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 def registrar_empleado():
     print("=== REGISTRO DE EMPLEADOS ===")
     id_empleado = input("ID del empleado: ")
@@ -7,12 +8,47 @@ def registrar_empleado():
     with open("empleados.txt", "a", encoding="utf-8") as archivo:
         archivo.write(linea)
     print("Empleado registrado con éxito.\n")
+       def validar_empleado(id_empleado):
+    try:
+        with open("empleados.txt", "r", encoding="utf-8") as archivo:
+            for linea in archivo:
+                if linea.startswith(id_empleado + ","):
+                    return True
+    except FileNotFoundError:
+        print("Error.")
+    return False
+def registrar_asistencia():
+    print("REGISTRO DE ASISTENCIAS")
+    id_empleado = input("ID del empleado: ")
+    if not validar_empleado(id_empleado):
+        print("ID no encontrado..\n")
+        return
+    tipo = input("entrada o salida? (E/S): ").strip().upper()
+    if tipo not in ["E", "S"]:
+        print("Error")
+        return
+    ahora = datetime.now()
+    fecha = ahora.strftime("%Y-%m-%d")
+    hora = ahora.strftime("%H:%M:%S")
+    linea = f"{id_empleado},{tipo},{fecha},{hora}\n"
+    with open("asistencias.txt", "a", encoding="utf-8") as archivo:
+        archivo.write(linea)
+    print("asistencia registrada.\n")
 def main():
     while True:
-        registrar_empleado()
-        otra = input("¿Deseas registrar otro empleado? (s/n): ")
-        if otra.lower() != 's':
-            print("Saliendo del registro.")
+        print("MENU PRINCIPAL")
+        print("1. registrar a un empleado")
+        print("2. asistencia")
+        print("3. Salir")
+        opcion = input("elige una opción (1/2/3): ")
+        if opcion == "1":
+            registrar_empleado()
+        elif opcion == "2":
+            registrar_asistencia()
+        elif opcion == "3":
+            print("saliendo.")
             break
+        else:
+            print("error\n")
 if __name__ == "__main__":
     main()
