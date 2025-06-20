a012f91c813a0d01000068d44ed3ed588b5571ae
Zempoalteca Erik Alonso <105683922+Zempoalteca-Erik-Alonso@users.noreply.github.com>
2025-06-19 19:28:30 -0600

    Update Control de asistencias.py

--- Control de asistencias.py
+++ Control de asistencias.py
@@ -34,18 +34,50 @@ def registrar_asistencia():
     with open("asistencias.txt", "a", encoding="utf-8") as archivo:
         archivo.write(linea)
     print("asistencia registrada.\n")
+def empleado_y_asistencias():
+    print("CHECAR EMPLEADO Y ASISTENCIAS")
+    id_empleado = input("Ingrese el ID del empleado: ")
+    try:
+        with open("empleados.txt", "r", encoding="utf-8") as emp:
+            encontrado = False
+            for linea in emp:
+                if linea.startswith(id_empleado + ","):
+                    print(f"empleado: {linea.strip()}")
+                    encontrado = True
+                    break
+            if not encontrado:
+                print("empleado no encontrado")
+                return
+    except FileNotFoundError:
+        print("error:")
+        return
+    print("\nhistorial de sus asistencias:")
+    try:
+        with open("asistencias.txt", "r", encoding="utf-8") as asist:
+            contador = 0
+            for linea in asist:
+                if linea.startswith(id_empleado + ","):
+                    print(f" - {linea.strip()}")
+                    contador += 1
+            if contador == 0:
+                print("no hay asistencias")
+    except FileNotFoundError:
+        print("no se ha registrado asistencia todavia")
 def main():
     while True:
         print("MENU PRINCIPAL")
         print("1. registrar a un empleado")
         print("2. asistencia")
-        print("3. Salir")
-        opcion = input("elige una opción (1/2/3): ")
+        print ("3. checar empleado y asistencias")
+        print("4. Salir")
+        opcion = input("elige una opción (1/2/3/4): ")
         if opcion == "1":
             registrar_empleado()
         elif opcion == "2":
             registrar_asistencia()
         elif opcion == "3":
+             empleado_y_asistencias()
+        elif opcion == "4":
             print("saliendo.")
             break
         else:
